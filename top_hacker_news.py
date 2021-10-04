e6419d572d6a5ec5959976851a12094c34149592
RogersKim <72293345+RogersKim@users.noreply.github.com>
2021-10-04 10:49:12 +0530

    Update top_hacker_news.py

--- top_hacker_news.py
+++ top_hacker_news.py
@@ -26,7 +26,8 @@ def create_custom_hn(links, subtext):
     vote = subtext[idx].select('.score')
     if len(vote):
       points = int(vote[0].getText().replace(' points', ''))
-      if points > 99:
+      
+      if points > 100: 
         hn.append({'title': title, 'link': href, 'votes': points})
   return sort_stories_by_votes(hn)
  
